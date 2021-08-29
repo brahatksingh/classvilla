b3b34377a9fee59436e81ef13465e7b7fb0a1828
BrahatSingh <brahatksingh@gmail.com>
2021-08-29 18:45:48 +0530

    Creating a new Branch. Deepali use this if possible.

--- app/src/main/java/com/brewingkotlin/classvilla/Activities/SignInActivity.kt
+++ app/src/main/java/com/brewingkotlin/classvilla/Activities/SignInActivity.kt
@@ -1,11 +1,12 @@
-package com.brewingkotlin.classvilla
+package com.brewingkotlin.classvilla.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brewingkotlin.classvilla.R
 
-class SignUpActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_sign_in)
     }
 }
