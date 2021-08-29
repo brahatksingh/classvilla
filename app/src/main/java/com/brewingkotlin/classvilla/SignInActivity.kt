6b5c3b8c912f2f0031eb3ba5886e30141a74cc5f
Hawk Itzme <smquasim016@gmail.com>
2021-08-29 17:17:19 +0530

    Cards Bg Added

--- app/src/main/java/com/brewingkotlin/classvilla/SignInActivity.kt
+++ app/src/main/java/com/brewingkotlin/classvilla/SignInActivity.kt
@@ -1,11 +1,18 @@
 package com.brewingkotlin.classvilla
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val logo: VideoView = findViewById(R.id.SignLogo)
+        val logouri: Uri = Uri.parse("android.resource://$packageName/${R.raw.signlogo}")
+        logo.setVideoURI(logouri)
+        logo.start()
     }
 }
